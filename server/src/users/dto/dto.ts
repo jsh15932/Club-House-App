0c7fa4fb7a525772a1a8fe06e42e854177a99f94
jsh15932 <jsh15932@jsh15932ui-MacBookPro.local>
2022-04-25 01:35:20 +0900

    Server:Add User Service

---
+++ server/src/users/dto/dto.ts
@@ -0,0 +1,13 @@
+export class userProfileDto {
+    id?: string;
+    email?: string;
+    password?: string;
+    user_id?: string;
+    lname?: string;
+    avatar?: string;
+    isActive?: boolean;
+    phone?: string;
+    code?: string;
+    created_at?: Date;
+    updated_at?: Date;
+}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserDto } from 'src/dto/user.dto';
+import { Repository } from 'typeorm';
+import { userProfileDto } from './dto/dto';
+import { User } from './entities/users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>
+    ) {}
+
+    async Create(user: userProfileDto): Promise<UserDto> {
+        const create = await this.userRepository.create(user);
+        const profile = await this.userRepository.save(create);
+        return profile;
+    }
+    
+}
